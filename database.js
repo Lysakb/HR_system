08b20ffc56b6b10084fa3325c743c5ac8c37f1b9
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-02-21 15:39:17 +0100

    connection to database

---
+++ database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+const MONGODB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL;
+
+function connectToMongodb(){
+    mongoose.connect(MONGODB_CONNECTION_URL)
+
+    mongoose.connection.on("connected", ()=>{
+        console.log("Connected to MongoDB")
+    })
+
+    mongoose.connection.on("error", (err)=>{
+        console.log("Error in connection!")
+    }) 
+}
+
+module.exports = {connectToMongodb};
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+const express = require("express");
+const {connectToMongodb} = require("./database");
+require("dotenv").config();
+
+connectToMongodb()
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(400).send(err.message)
+})
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Home Page!!")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening at ${PORT}`)
+})
