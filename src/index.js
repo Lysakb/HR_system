c3e1fbffde290186d784062aa39f73c11ce75067
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-02-23 15:34:32 +0100

    database connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {connectToMongodb} = require("./database");
+const {connectToMongodb} = require("../config/database");
 require("dotenv").config();
 
 connectToMongodb()
