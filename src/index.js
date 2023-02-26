9b5856468f7745a9c7924052de75ffc72968fe4a
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-02-26 17:34:43 +0100

    error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const {connectToMongodb} = require("../config/database");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const errorHandler = require("../middleware/errorHandler");
 require("dotenv").config();
 
 connectToMongodb()
@@ -9,6 +13,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(helmet());
+app.use(errorHandler());
+app.use(morgan('dev'));
+app.use(cors());
 
 app.use((err, req, res, next)=>{
     console.log(err);
